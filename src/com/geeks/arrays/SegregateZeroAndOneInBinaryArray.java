9c4d7010dab844a718fa4df45cb76a9ca907ab1e
Tejas Rane <tejas.m.rane@gmail.com>
2017-12-02 00:23:42 -0800

    segregate

--- src/com/geeks/arrays/SegregateZeroAndOneInBinaryArray.java
+++ src/com/geeks/arrays/SegregateZeroAndOneInBinaryArray.java
@@ -6,22 +6,28 @@ public class SegregateZeroAndOneInBinaryArray {
 		int index1= 0 ;
 		int index2= input.length-1;
 		
-		while(index1 < index2){
-			if(input[index1] == 0){
+		while(index1 <= index2){
+			while(input[index1] == 0){
 				index1++;
-			}else {
-				
 			}
 				
-			if(input[index2]==1){
+			while(input[index2]==1){
 				index2--;
 			}
+			int temp = input[index1];
+			input[index1] = input[index2];
+			input[index2]= temp;
+			
+		}
+		
+		for (int i : input) {
+			System.out.print(i+ " ");
 		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int arr1[] =  new int[] {0,1,0,1,1,1,0,0,0,1,0};
+		int arr1[] =  new int[] {0,1,0,1,1,1,0,0,0,1,0,1};
 		segregate(arr1);
 	}
 
