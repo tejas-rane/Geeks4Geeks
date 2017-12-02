d2bd1ee37b3e249aaf274884330a0bd01d43e6ec
Tejas Rane <tejas.m.rane@gmail.com>
2017-12-02 00:36:15 -0800

    min diff

---
+++ src/com/geeks/arrays/FindMinDIffInAnyTwoElemntsInArray.java
@@ -0,0 +1,20 @@
+package com.geeks.arrays;
+
+import java.util.Arrays;
+
+public class FindMinDIffInAnyTwoElemntsInArray {
+
+	public static void findMin(int arr[]){
+		int diff = Integer.MAX_VALUE;
+		
+		Arrays.sort(arr);
+		for(int i =0; i < arr.length;i++){
+			
+		}
+	}
+	public static void main(String[] args) {
+		int arr[] = {1, 5, 3, 19, 18, 25}; //Minimum difference is between 18 and 19
+		int arr1[] =  new int[] {1, 19, -4, 31, 38, 25, 100};//Minimum difference is between 1 and -4
+		
+	}
+}
